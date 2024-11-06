defc008323c3a151e91764e4d40b72309e6a685d
Felipe <feelipefst@gmail.com>
2024-11-06 00:47:18 -0300

    final

---
+++ auto/city.py
@@ -0,0 +1,23 @@
+"""Cidades:
+Cajazeiras                  1288: 86
+Itaporanga
+São José de Piranhas        1429: 99
+Bonito de Santa Fé          1274: 97
+São João do Rio do Peixe    1249: 122
+Boa Ventura                 1270: 98
+Baixio                      910: 101
+Bom Jesus                   1272: 103
+Carrapateira                1295: 104
+Conceição                   1300: 105
+Ipaumirim                   969: 106
+Marizopolis                 1355: 107
+Monte Horebe                1363: 108
+Poço José de Moura          1390: 109
+Santa Inês                  1410: 111
+Santa Helena                1409: 110
+São José de Caiana
+Serra Grande
+Triunfo                     1458: 115
+Umari                       1067: 116
+Aurora                      909: 128
+"""
---
+++ auto/test.py
@@ -0,0 +1,6 @@
+import time
+import pyautogui
+
+time.sleep(2)
+pyautogui.click(x=943, y=301)
+pyautogui.hotkey('esc')
